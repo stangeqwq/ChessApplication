48f6568a76ff920a660102db60efb219e2f33edb
stangeqwq <StangelandEricJoshua@gmail.com>
2024-02-15 22:05:04 +0100

    starting to code

--- src/main/java/Chess/Bishop.java
+++ src/main/java/Chess/Bishop.java
@@ -1,5 +1,5 @@
 package Chess;
 
-public class Bishop {
+public class Bishop implements ChessPiece {
     
 }
--- src/main/java/Chess/Player.java
+++ src/main/java/Chess/Player.java
@@ -1,8 +1,26 @@
 package Chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
     private boolean isWhite = true;
+    private List<ChessPiece> pieces = new ArrayList<ChessPiece>();
+    private Character[] validColumns = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    private int[] validRows = {'1', '2', '3', '4', '5', '6', '7', '8'};
     
+    public Player(boolean isWhite) {
+        this.isWhite = isWhite;
+        if (isWhite) {
+            for (int i = 0; i < 8; i++) {
+                pieces.add(new Pawn(String.format("%c%i", validColumns[i], 2)));
+            }
+        } else {
+            for (int i = 0; i < 8; i++) {
+                pieces.add(new Pawn(String.format("%c%i", validColumns[i], 2)));
+            }
+        }
+    }
     public static void main(String args[]) {
         System.out.println("Hello world!");
     }
