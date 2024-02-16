9c1aa04da2decee511b4551a0543a29547c29ccc
stangeqwq <StangelandEricJoshua@gmail.com>
2024-02-16 12:08:13 +0100

    d

--- src/main/java/Chess/ChessPiece.java
+++
@@ -1,7 +0,0 @@
-package Chess;
-
-public interface ChessPiece {
-    public void setPosition(String);
-    public void getPosition();
-    private boolean isValidPosition(String);
-}
--- src/main/java/Chess/King.java
+++
@@ -1,5 +0,0 @@
-package Chess;
-
-public class King implements ChessPiece{
-    
-}
