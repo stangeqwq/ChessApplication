7f9ab5cfff13ec4078b24bd5bc68fe154fa2e2b5
stangeqwq <StangelandEricJoshua@gmail.com>
2024-02-16 08:49:29 +0100

    first commit of the day

---
+++ src/main/java/Chess/ChessGame.java
@@ -0,0 +1,10 @@
+package Chess;
+
+public class ChessGame {
+    private Player player1;
+    private Player player2;
+    public ChessGame(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+}
