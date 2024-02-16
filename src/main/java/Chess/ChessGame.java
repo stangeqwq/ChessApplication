53bb99547dfea90b29f55b34fa7bfeecb267c3a9
stangeqwq <StangelandEricJoshua@gmail.com>
2024-02-16 10:00:58 +0100

    coding player

--- src/main/java/Chess/ChessGame.java
+++ src/main/java/Chess/ChessGame.java
@@ -1,10 +1,14 @@
 package Chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessGame {
     private Player player1;
     private Player player2;
-    public ChessGame(Player player1, Player player2) {
-        this.player1 = player1;
-        this.player2 = player2;
+    private List<List<String>> chessBoard = new ArrayList<List<String>>();
+    public ChessGame(String player1, String player2) {
+        this.player1 = new Player(true);
+        this.player2 = new Player(false);
     }
 }
--- src/test/java/Chess/ChessTest.java
+++ src/test/java/Chess/ChessTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class ChessTest {
+    @BeforeEeach
+    public void setup() {
+        ChessGame1 = new ChessGame("Alice", "Bob");
+    }
     @Test
     public void testBasic() {
         String test = "This is a test";
@@ -13,6 +17,7 @@ public class ChessTest {
     
     @Test
     public void testRightSetup() {
+        
         Assertions.assertTrue(true==true, "nice");
     }
 }
