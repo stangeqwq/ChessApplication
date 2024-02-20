103a98ee7496bb45e3beca1fc3f2351c89aa578e
stangeqwq <StangelandEricJoshua@gmail.com>
2024-02-20 13:35:39 +0100

    finished rook quickly (copy paste bishop. but with edits

--- src/main/java/Chess/Rook.java
+++ src/main/java/Chess/Rook.java
@@ -26,11 +26,147 @@ public class Rook implements ChessPiece {
         }
     }
 
+    private boolean isOccupied(String toPosition) {
+        for (ChessPiece piece : this.getOwner().getOpponent().getPieces()) { // check enemy pieces
+            if (piece.getPosition().equals(toPosition)) {
+                return true;
+            }
+
+        }
+        for (ChessPiece piece : this.getOwner().getPieces()) { // checks friendly pieces
+            if (piece.getPosition().equals(toPosition)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private boolean isEnemy(String toPosition) {
+        for (ChessPiece piece : this.getOwner().getOpponent().getPieces()) {
+            if (piece.getPosition().equals(toPosition)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<String> getValidPositionsTo(String position) {
+        // cross pattern
+        List<String> validPositionsTo = new ArrayList<String>();
+        // right
+        String currentPositionCheck = this.position;
+        while (currentPositionCheck.charAt(0) < 'h'
+                && (!isOccupied(currentPositionCheck) || currentPositionCheck.equals(this.getPosition()))) { // ignore
+                                                                                                             // first
+                                                                                                             // case of
+                                                                                                             // currentposition
+                                                                                                             // (bug)
+            Character column = currentPositionCheck.charAt(0);
+            Character row = currentPositionCheck.charAt(1);
+            column = (char) (column + 1);
+            String toPosition = Character.toString(column) + Character.toString(row);
+            if (!isOccupied(toPosition)) {
+                validPositionsTo.add(toPosition);
+            } else if (isEnemy(toPosition)) {
+                // add first occupied square if enemy position's with specification 'x' for
+                // capturing
+                validPositionsTo.add(Character.toString('x') + toPosition);
+            }
+            currentPositionCheck = toPosition;
+        }
+        // left
+        currentPositionCheck = this.position;
+        while (currentPositionCheck.charAt(0) > 'a'
+                && (!isOccupied(currentPositionCheck) || currentPositionCheck.equals(this.getPosition()))) {
+            Character column = currentPositionCheck.charAt(0);
+            Character row = currentPositionCheck.charAt(1);
+            column = (char) (column - 1);
+            String toPosition = Character.toString(column) + Character.toString(row);
+            if (!isOccupied(toPosition)) {
+                validPositionsTo.add(toPosition);
+            } else if (isEnemy(toPosition)) {
+                // add first occupied square if enemy position's with specification 'x' for
+                // capturing
+                validPositionsTo.add(Character.toString('x') + toPosition);
+            }
+            currentPositionCheck = toPosition;
+        }
+        // up
+        currentPositionCheck = this.position;
+        while (currentPositionCheck.charAt(1) < '8'
+                && (!isOccupied(currentPositionCheck) || currentPositionCheck.equals(this.getPosition()))) {
+            Character column = currentPositionCheck.charAt(0);
+            Character row = currentPositionCheck.charAt(1);
+            row = (char) (row + 1);
+            String toPosition = Character.toString(column) + Character.toString(row);
+            if (!isOccupied(toPosition)) {
+                validPositionsTo.add(toPosition);
+            } else if (isEnemy(toPosition)) {
+                // add first occupied square if enemy position's with specification 'x' for
+                // capturing
+                validPositionsTo.add(Character.toString('x') + toPosition);
+            }
+            currentPositionCheck = toPosition;
+        }
+        // down
+        currentPositionCheck = this.position;
+        while (currentPositionCheck.charAt(1) > '1'
+                && (!isOccupied(currentPositionCheck) || currentPositionCheck.equals(this.getPosition()))) {
+            Character column = currentPositionCheck.charAt(0);
+            Character row = currentPositionCheck.charAt(1);
+            row = (char) (row - 1);
+            String toPosition = Character.toString(column) + Character.toString(row);
+            if (!isOccupied(toPosition)) {
+                validPositionsTo.add(toPosition);
+            } else if (isEnemy(toPosition)) {
+                // add first occupied square if enemy position's with specification 'x' for
+                // capturing
+                validPositionsTo.add(Character.toString('x') + toPosition);
+            }
+            currentPositionCheck = toPosition;
+        }
+        return validPositionsTo;
+    }
+
     public boolean isValidPosition(String move) {
+        boolean capturing = false;
+        String position = move.substring(move.length() - 2);
         if (Character.isLowerCase(move.charAt(0))) {
             return false; // it is a pawn
-        } else {
+        } else if (move.charAt(0) == this.getInitial()) {
+            if (move.length() >= 4) { // f.e. Rxe5 or Rdxe4 or Rde4
+                if (move.charAt(move.length() - 3) == 'x') {
+                    capturing = true;
+                } else if ((move.charAt(move.length() - 3) == this.getPosition().charAt(0))
+                        || (move.charAt(move.length() - 3) == this.getPosition().charAt(1))) {
+                    // specified which rook piece so we check if this rook is correct (for both
+                    // row and column)
+                    // do nothing we continue below
 
+                } else {
+                    return false; // this is the wrong bishop (different column / row to specified)
+                }
+            }
+            List<String> validPositionsTo = getValidPositionsTo(this.getPosition());
+            if (!capturing) { // position must not be occupied and in validPositionsTo
+                for (String validPositionTo : validPositionsTo) {
+                    if (validPositionTo.equals(position)) {
+                        return true;
+                    }
+                }
+            } else { // we check including the "x" pieces but this time specifying only position
+                     // interested
+                for (String validPositionTo : validPositionsTo) {
+                    if (validPositionTo.substring(validPositionTo.length() - 2).equals(position)
+                            && validPositionTo.length() == 3) { // actually have to capture a piece when capturing
+                        return true;
+                    }
+                }
+
+            }
+        } else { // not same initial to bishop
+            return false;
         }
         return false;
     }
