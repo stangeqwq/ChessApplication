eec9df2e82baa7c65aa0129caaa6dbfa2002f56d
stangeqwq <StangelandEricJoshua@gmail.com>
2024-02-20 12:41:42 +0100

    i think im finished with bishop?

--- src/main/java/Chess/Bishop.java
+++ src/main/java/Chess/Bishop.java
@@ -28,16 +28,25 @@ public class Bishop implements ChessPiece {
 
     private boolean isOccupied(String toPosition) {
         for (ChessPiece piece : this.getOwner().getOpponent().getPieces()) { // check enemy pieces
-            if (piece.getPosition() == toPosition) {
+            if (piece.getPosition().equals(toPosition)) {
                 return true;
             }
 
         }
         for (ChessPiece piece : this.getOwner().getPieces()) { // checks friendly pieces
-            if (piece.getPosition() == toPosition) {
+            if (piece.getPosition().equals(toPosition)) {
                 return true;
             }
 
+        }
+        return false;
+    }
+
+    private boolean isEnemy(String toPosition) {
+        for (ChessPiece piece : this.getOwner().getOpponent().getPieces()) {
+            if (piece.getPosition() == toPosition) {
+                return true;
+            }
         }
         return false;
     }
@@ -47,7 +56,8 @@ public class Bishop implements ChessPiece {
         List<String> validPositionsTo = new ArrayList<String>();
         // up right
         String currentPositionCheck = this.position;
-        while (currentPositionCheck.charAt(0) < 'h' && currentPositionCheck.charAt(1) < '8') {
+        while (currentPositionCheck.charAt(0) < 'h' && currentPositionCheck.charAt(1) < '8'
+                && !isOccupied(currentPositionCheck)) {
             Character column = currentPositionCheck.charAt(0);
             Character row = currentPositionCheck.charAt(1);
             column = (char) (column + 1);
@@ -55,30 +65,103 @@ public class Bishop implements ChessPiece {
             String toPosition = Character.toString(column) + Character.toString(row);
             if (!isOccupied(toPosition)) {
                 validPositionsTo.add(toPosition);
+            } else if (isEnemy(toPosition)) {
+                // add first occupied square if enemy position's with specification 'x' for
+                // capturing
+                validPositionsTo.add(Character.toString('x') + toPosition);
             }
-
+            currentPositionCheck = toPosition;
         }
         // up left
-        while (currentPositionCheck.charAt(0) > 'a' && currentPositionCheck.charAt(1) < '8') {
-
+        while (currentPositionCheck.charAt(0) > 'a' && currentPositionCheck.charAt(1) < '8'
+                && !isOccupied(currentPositionCheck)) {
+            Character column = currentPositionCheck.charAt(0);
+            Character row = currentPositionCheck.charAt(1);
+            column = (char) (column - 1);
+            row = (char) (row + 1);
+            String toPosition = Character.toString(column) + Character.toString(row);
+            if (!isOccupied(toPosition)) {
+                validPositionsTo.add(toPosition);
+            } else if (isEnemy(toPosition)) {
+                // add first occupied square if enemy position's with specification 'x' for
+                // capturing
+                validPositionsTo.add(Character.toString('x') + toPosition);
+            }
+            currentPositionCheck = toPosition;
         }
         // down right
-        while (currentPositionCheck.charAt(0) < 'h' && currentPositionCheck.charAt(1) > '1') {
-
+        while (currentPositionCheck.charAt(0) < 'h' && currentPositionCheck.charAt(1) > '1'
+                && !isOccupied(currentPositionCheck)) {
+            Character column = currentPositionCheck.charAt(0);
+            Character row = currentPositionCheck.charAt(1);
+            column = (char) (column + 1);
+            row = (char) (row - 1);
+            String toPosition = Character.toString(column) + Character.toString(row);
+            if (!isOccupied(toPosition)) {
+                validPositionsTo.add(toPosition);
+            } else if (isEnemy(toPosition)) {
+                // add first occupied square if enemy position's with specification 'x' for
+                // capturing
+                validPositionsTo.add(Character.toString('x') + toPosition);
+            }
+            currentPositionCheck = toPosition;
         }
         // down left
-        while (currentPositionCheck.charAt(0) > 'a' && currentPositionCheck.charAt(1) > '1') {
-
+        while (currentPositionCheck.charAt(0) > 'a' && currentPositionCheck.charAt(1) > '1'
+                && !isOccupied(currentPositionCheck)) {
+            Character column = currentPositionCheck.charAt(0);
+            Character row = currentPositionCheck.charAt(1);
+            column = (char) (column - 1);
+            row = (char) (row - 1);
+            String toPosition = Character.toString(column) + Character.toString(row);
+            if (!isOccupied(toPosition)) {
+                validPositionsTo.add(toPosition);
+            } else if (isEnemy(toPosition)) {
+                // add first occupied square if enemy position's with specification 'x' for
+                // capturing
+                validPositionsTo.add(Character.toString('x') + toPosition);
+            }
+            currentPositionCheck = toPosition;
         }
         return validPositionsTo;
     }
 
     public boolean isValidPosition(String move) { // takes a position without an initial
+        boolean capturing = false;
+        String position = move.substring(move.length() - 2);
         if (Character.isLowerCase(move.charAt(0))) {
             return false; // it is a pawn
         } else if (move.charAt(0) == this.getInitial()) {
+            if (move.length() >= 4) { // f.e. Bxe5 or Bdxe4 or Bde4
+                if (move.charAt(move.length() - 3) == 'x') {
+                    capturing = true;
+                } else if ((move.charAt(move.length() - 3) == this.getPosition().charAt(0))
+                        || (move.charAt(move.length() - 3) == this.getPosition().charAt(1))) {
+                    // specified which bishop piece so we check if this bishop is correct (for both
+                    // row and column)
+                    // do nothing we continue below
+
+                } else {
+                    return false; // this is the wrong bishop (different column / row to specified)
+                }
+            }
             List<String> validPositionsTo = getValidPositionsTo(this.getPosition());
-        } else {
+            if (!capturing) { // position must not be occupied and in validPositionsTo
+                for (String validPositionTo : validPositionsTo) {
+                    if (validPositionTo.equals(position)) {
+                        return true;
+                    }
+                }
+            } else { // we check including the "x" pieces but this time specifying only position
+                     // interested
+                for (String validPositionTo : validPositionsTo) {
+                    if (validPositionTo.substring(validPositionTo.length() - 2).equals(position)) {
+                        return true;
+                    }
+                }
+
+            }
+        } else { // not same initial to bishop
             return false;
         }
         return false;
@@ -88,6 +171,7 @@ public class Bishop implements ChessPiece {
         if (isValidPosition(move)) {
             // we only take "a4" f.e. type not whole position "Ndxe4" this must be validated
             this.position = move.substring(move.length() - 2);
+            this.getOwner().getOpponent().removePieceAtPosition(this.position);
         }
     }
 
