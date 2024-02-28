a4c37f01f43a91ff73e17bc382cb1535305e3254
stangeqwq <StangelandEricJoshua@gmail.com>
2024-02-28 21:08:29 +0100

    ed

---
+++ README.md
@@ -0,0 +1,17 @@
+Dette var innleveringen min som prosjekt for faget objektorientert programmering vår 2024 NTNU.
+# Chess Application
+This application allows users to create chess games, save those chess games, and later load the chess games.
+It uses a text input that takes in a move, in chess notation, and displays the board after the move. The move list is also given. 
+
+## Main Interface
+Below is how the application looks like when actually trying to make moves.
+![MainGame.png](images/MainGame.png)
+
+## Home Screen
+The first interface the user is met with.
+![HomeScreen.png](images/Homescreen.png)
+
+## Loaded Games
+Below shows how the user is able to access games that have been saved.
+![LoadedGames.png](images/LoadedGames.png)
+
---
+++ src/main/java/Chess/WelcomeScreen.java
@@ -0,0 +1,69 @@
+package Chess;
+
+import java.io.IOException;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class WelcomeScreen {
+    @FXML
+    private Button New;
+    @FXML
+    private Button Load;
+    @FXML
+    private Button Exit;
+
+    @FXML
+    private void NewGame() {
+        // Load the FXML file of the new scene
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("StartChess.fxml"));
+            Parent root = loader.load();
+
+            // Create a new Scene with the loaded FXML content
+            Scene newScene = new Scene(root);
+
+            // Get the current stage (window)
+            Stage stage = (Stage) New.getScene().getWindow();
+
+            // Switch to the new scene
+            stage.setScene(newScene);
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+
+    }
+
+    @FXML
+    private void LoadGame() {
+        // fetch from database and then load the state by setting each move
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("LoadScreen.fxml"));
+            Parent root = loader.load();
+
+            // Create a new Scene with the loaded FXML content
+            Scene newScene = new Scene(root);
+
+            // Get the current stage (window)
+            Stage stage = (Stage) New.getScene().getWindow();
+
+            // Switch to the new scene
+            stage.setScene(newScene);
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
+    }
+
+    @FXML
+    private void ExitApplication() {
+        Platform.exit();
+        System.exit(0);
+    }
+}
