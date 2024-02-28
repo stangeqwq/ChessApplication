6b3f40c74f3671004a5dfff5e7644b95a6d98740
stangeqwq <StangelandEricJoshua@gmail.com>
2024-02-28 14:38:19 +0100

    working chess main game  (ugly ui though)

--- src/main/java/Chess/MainGameController.java
+++ src/main/java/Chess/MainGameController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
-
+import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class MainGameController {
@@ -16,18 +16,30 @@ public class MainGameController {
     private TextField inputMove;
     @FXML
     private TextArea Board;
+    @FXML
+    private Button OkButton;
 
     private String move;
 
+    private ChessGame game1;
+
     @FXML
-    private void initialize() {
-        ChessGame game1 = new ChessGame("Alice", "Bob");
+    void initialize() {
+        game1 = new ChessGame("Alice", "Bob");
         Board.clear();
         Board.appendText(game1.getBoard());
     }
 
     @FXML
-    private void handleMoveInput() {
-        move = inputMove.getText();
+    private void handleInputMove() {
+        try {
+            move = inputMove.getText();
+            game1.move(move);
+            Board.clear();
+            Board.appendText(game1.getBoard());
+        } catch (Exception event) {
+            System.out.println(event);
+
+        }
     }
 }
